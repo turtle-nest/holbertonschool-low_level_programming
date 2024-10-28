72955dcbc1e9342f075d2d6483883e415a214475
turtle-bitch <n78693107@gmail.com>
2024-10-28 13:56:04 +0000

    TASK 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 /**
-*puts function
-*/
+ * puts - prints characters
+ * @parameterx: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: 
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
