ae43f38c949d5ae3e8fe622f4632a0f28774693c
turtle-bitch <n78693107@gmail.com>
2024-10-28 14:14:30 +0000

    TASK 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 /**
-*main
-*
-*/
+ * main - Short description, single line
+ * @void: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
 int main (void)
 {
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
