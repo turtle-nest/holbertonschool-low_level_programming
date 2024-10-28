cbb168e755a44103cbcc478e4d744c02a4b4347a
turtle-bitch <n78693107@gmail.com>
2024-10-28 14:11:41 +0000

    TASK 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-/*
-*
-*/
+
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
