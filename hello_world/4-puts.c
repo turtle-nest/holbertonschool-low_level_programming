513228215320243e7964c81a5226fb67c033d591
turtle-bitch <n78693107@gmail.com>
2024-10-28 10:21:40 +0000

    TASK 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,7 @@
-#!/bin/bash
-gcc -c puts.c
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    puts("Programming is like building a multilingual puzzle\n");
+    return 0;
+}
