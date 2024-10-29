358b831ca5cfb9b128f07ca1222932f78c544d88
turtle-nest <n78693107@gmail.com>
2024-10-29 08:41:23 +0000

    TASK 0

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+ /**
+  * main -  check the code
+  * @void:
+ (* a blank line
+  * Description: Longer description of the function)?
+ (* section header: Section description)*
+  * Return: Description of
+  */
+int _isupper(int c);
+
+int main(void)
+{
+    char c;
+    
+    if (_isupper(c))
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+  }
