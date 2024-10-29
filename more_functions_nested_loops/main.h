84f781f259d18ef9dc4da8e128c4ecbc60c3d016
turtle-nest <n78693107@gmail.com>
2024-10-29 15:06:12 +0100

    Update main.h

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,17 +1,8 @@
 #ifndef main_h
 #define main_h
-
 /**
- * _isupper - checks for uppercase character
- * @c: argument checked
- *
- * Return: 1 if c uppercase otherwise 0.
- */
-int _isupper(int c)
-{
-    if (c >= 'A' && c <= 'Z')
-        return (1);
-    else
-        return (0);
-}
+* Prototypes des fonctions
+*/
+int _isupper(int c);
+
 #endif /* main.h */
