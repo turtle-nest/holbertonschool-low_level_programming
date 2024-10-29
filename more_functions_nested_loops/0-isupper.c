d58a01d42678de03482ee592da932b128ea2bbb9
turtle-nest <n78693107@gmail.com>
2024-10-29 15:07:38 +0100

    Update 0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
 #include "main.h"
 
- /**
-  * main -  check the code
-  * @void:
-  * Description: checks the function
-  * Return: 1 or 0.
-  */
-int _isupper(int c);
-
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
     char c;
-    
-    if (_isupper(c))
-    {
-        return (1);
-    }
-    else
-    {
-        return (0);
-    }
-  }
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
+}
