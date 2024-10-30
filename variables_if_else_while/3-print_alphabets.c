eb63894d1be04e7b7e4b11ebcee6a0faaaf26c49
turtle-nest <n78693107@gmail.com>
2024-10-30 10:13:56 +0000

    TASK 2

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - checks the instructions
+ * @void: none
+ *
+ * Description: 
+(* section header: Section description)*
+ * Return: none
+ */
+
