6e75f2c33a9a22e941dfc38f931f198c4b45952a
turtle-nest <n78693107@gmail.com>
2024-10-30 09:56:42 +0000

    TASK 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -9,4 +9,18 @@
 (* section header: Section description)*
  * Return: none
  */
+int main(void)
+{
+	int n;
 
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	n = n % 10;
+	if (n < 5)
+		printf("%d and is greater than 5", n);
+	if (n == 0)
+		printf("%d and is 0", n);
+	else 
+		printf("%d and is less than 6 and not 0", n);
+	return (0);
+}
