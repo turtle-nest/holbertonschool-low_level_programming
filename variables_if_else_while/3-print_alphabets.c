d0b53fc3e1f5c0d604315063d5db96be0d0c2512
turtle-nest <n78693107@gmail.com>
2024-10-30 10:23:03 +0000

    TASK 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -5,8 +5,21 @@
  * main - checks the instructions
  * @void: none
  *
- * Description: 
+ * Description: prints the alphabet in lowercase, and then in uppercase, followed by a new line.
 (* section header: Section description)*
  * Return: none
  */
+int main(void)
+{
+    char letter;
 
+    for (letter = 'a'; letter <= 'z'; letter++)
+        putchar(letter);
+
+    for (letter = 'A'; letter <= 'Z'; letter++)
+        putchar(letter);
+
+    putchar('\n');
+
+    return 0;
+}
