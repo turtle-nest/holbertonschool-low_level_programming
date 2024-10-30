77d7ff93c9809288fb723ef9cd7812cede760bc8
turtle-nest <n78693107@gmail.com>
2024-10-30 17:34:29 +0000

    TASK 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -2,8 +2,24 @@
  * main - checks the instructions
  * @void: none
  *
- * Description:
+ * Description:prints all possible combinations of single-digit numbers.
 (* section header: Section description)*
  * Return: none
  */
+#include <stdio.h>
+
+int main(void) {
+    int i;
+
+    for (i = 0; i < 10; i++) {
+        putchar(i + '0');
+        if (i < 9) {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+    putchar('\n');
+
+    return 0;
+}
 
