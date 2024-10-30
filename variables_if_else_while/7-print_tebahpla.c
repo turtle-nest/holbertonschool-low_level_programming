368704720d53941b928d2315e0e915d7a14a6635
turtle-nest <n78693107@gmail.com>
2024-10-30 14:27:19 +0000

    TASK 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - checks the instructions
+ * @void: none
+ *
+ * Description: prints the lowercase alphabet in reverse, followed by a new line.
+(* section header: Section description)*
+ * Return: none
+ */
+{
+    char letter;
+
+    for (letter = 'z'; letter >= 'a'; letter--)
+        putchar(letter);
+
+    putchar('\n');
+
+    return 0;
+}
