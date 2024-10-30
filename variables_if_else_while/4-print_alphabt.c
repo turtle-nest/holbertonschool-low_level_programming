0187c1911e7e7ed260a6742e6c3e55e804d209e3
turtle-nest <n78693107@gmail.com>
2024-10-30 13:48:50 +0000

    TASK 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - checks the instructions
+ * @void: none
+ *
+ * Description: prints the alphabet in lowercase, followed by a new line.
+(* section header: Section description)*
+ * Return: none
+ */
+int main(void)
+{
+    char letter;
+
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        if (letter != 'q' && letter != 'e')
+            putchar(letter);
+    }
+
+    putchar('\n');
+
+    return 0;
+}
+
