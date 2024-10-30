87c35fcc7edc4d284b3edc5174a4ec551e701ac0
turtle-nest <n78693107@gmail.com>
2024-10-30 17:47:34 +0000

    TASK 12

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+/**
+ * main - checks the instructions
+ * @void: none
+ *
+ * Description: prints all possible combinations of two two-digit numbers.
+(* section header: Section description)*
+ * Return: none
+ */
+#include <stdio.h>
+
+int main(void) {
+    int i, j;
+
+    for (i = 0; i < 100; i++) {
+        for (j = i + 1; j < 100; j++) {
+            putchar(i / 10 + '0');
+            putchar(i % 10 + '0');
+            putchar(' ');
+            putchar(j / 10 + '0');
+            putchar(j % 10 + '0');
+            if (i < 98 || j < 99) {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+    putchar('\n');
+
+    return 0;
+}
+
