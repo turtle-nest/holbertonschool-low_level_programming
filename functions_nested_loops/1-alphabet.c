b79464250c902782f9091206bfc3ed7ee5b598e6
turtle-nest <n78693107@gmail.com>
2024-10-31 20:27:49 +0100

    Update 1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,36 +1,20 @@
+#include "main.h"
 /**
  * main - checks the instructions
  * @void: none
  *
- * Description: prints the alphabet, in lowercase, followed by a new line. 
- * (* section header: Section description)*
+ * Description: prints the alphabet, in lowercase, followed by a new line.
+(* section header: Section description)*
  * Return: none
  */
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include "main.h"
-#include <unistd.h>
-
-#include <unistd.h>
-
-int _putchar(char c);
-
-void print_alphabet(void)
-{
-    char *alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-    while (*alphabet)
-    {
-        _putchar(*alphabet);
-        alphabet++;
-    }
-    _putchar('\n');
-}
 
 int main(void)
 {
-    print_alphabet();
-    return (0);
-}
+    
+    int main(void)
+    {
+        print_alphabet();
+        return (0);
+    }
 
+}
