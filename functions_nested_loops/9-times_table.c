f022ca34ae95f105bca638d6c5f0be4f984edc7c
turtle-nest <n78693107@gmail.com>
2024-10-31 10:38:59 +0000

    TASK 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,10 +1,15 @@
 /**
  * main - checks the instructions
- * @void: none
+ * @void: type int
  *
- * Description: 
- * (* section header: Section description)*
- * Return: none
+ * Description: a function that prints the 9 times table, starting with 0.
+(* section header: Section description)*
+ * Return: Always return 0.
  */
 #include "main.h"
+int main(void)
+{
+    times_table();
+        return (0);
+}
 
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -105,4 +105,34 @@ void jack_bauer(void)
         }
     }
 }
+/**
+ * TASK 9
+ *void times_table(void) : a function that prints the 9 times table, starting with 0.
+ */
+void times_table(void)
+{
+    int i, j;
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+            int product = i * j;
+            if (j != 0)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+            if (product < 10 && j != 0)
+            {
+                _putchar(' ');
+            }
+            if (product >= 10)
+            {
+                _putchar((product / 10) + '0');
+            }
+            _putchar((product % 10) + '0');
+        }
+        _putchar('\n');
+    }
+}
 #endif
