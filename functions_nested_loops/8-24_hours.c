ebbfc334f2f3337e17d5f5d0fbb292b6111b66f7
turtle-nest <n78693107@gmail.com>
2024-10-31 10:26:18 +0000

    TASK 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,10 +1,14 @@
 /**
  * main - checks the instructions
- * @void: none
+ * @void: type int
  *
- * Description: 
- * (* section header: Section description)*
- * Return: none
- */
-#include "main.h"
+ * Description: a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+(* section header: Section description)*
+ * Return: Always return 0.
+ */#include "main.h"
+
+int main(void) {
+    jack_bauer();
+        return (0);
+}
 
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -80,4 +80,29 @@ int _abs(int n)
     }
     return n;
 }
+/**
+ * TASK 7
+ *
+ */
+/**
+ * TASK 8
+ *void jack_bauer(void) : a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ */
+void jack_bauer(void)
+{
+    int hour, minute;
+
+    for (hour = 0; hour < 24; hour++)
+    {
+        for (minute = 0; minute < 60; minute++)
+        {
+            _putchar((hour / 10) + '0');
+            _putchar((hour % 10) + '0');
+            _putchar(':');
+            _putchar((minute / 10) + '0');
+            _putchar((minute % 10) + '0');
+            _putchar('\n');
+        }
+    }
+}
 #endif
