8bdb9378bb9e83ced9e1cdd86d91455006cbfc04
turtle-nest <n78693107@gmail.com>
2024-11-01 18:03:34 +0100

    Update _putchar.c

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,21 +1,6 @@
-/**
- * main - checks the instructions
- * @void: none
- *
- * Description:  prints _putchar, followed by a new line.
- * (* section header: Section description)*
- * Return: none
- */
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #include "main.h"
-#include <unistd.h>
 
-int main(void)
+int _putchar(char c)
 {
-    char msg[] = "_putchar\n";
-    write(1, msg, sizeof(msg) - 1);
-    return (0);
+    return write(1, &c, 1);
 }
-
