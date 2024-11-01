521970ae2e90eaf22876510778a263f2474472f2
turtle-nest <n78693107@gmail.com>
2024-11-01 17:24:42 +0100

    Update 1-alphabet.h

--- functions_nested_loops/1-alphabet.h
+++ functions_nested_loops/1-alphabet.h
@@ -1,9 +1,14 @@
 #ifndef 1-alphabet_h
 #define 1-alphabet_h
+#include <unistd.h>
 /**
  * TASK 1
  * print_alphabet_x10 : prints the alphabet, in lowercase, followed by a new line.
  */
+int _putchar(char c) 
+{ 
+    return write(1, &c, 1); 
+}
 void print_alphabet(void)
 {
     char letter = 'a';
