48160e4a959bf1104650cef29f25bd84c096f375
turtle-nest <n78693107@gmail.com>
2024-11-02 14:07:55 +0000

    TASK 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_isdigit - checks for a digit
+ *@c: the character to be checked
+ *
+ * Description: a function that checks for a digit (0 through 9)
+ *
+ * Return: 1 if the character is a digit, 0 otherwise.
+ */
+{
+    if (c >= '0' && c <= '9')
+    {
+        return (1);
+    }
+    return (0);
+}
