490f45dbf0682071815b24a181ca99ee2d8bfd43
turtle-nest <n78693107@gmail.com>
2024-11-02 13:13:37 +0000

    TASK 3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,25 +1,17 @@
+#include "main.h"
 /**
- * main - checks the instructions
- * @void: type int
+ * int _islower - checks the instructions
+ * @int: type int
  *
- * Description:
+ * Description:a function that checks for lowercase character.
 (* section header: Section description)*
  * Return: Always return 0.
  */
-#include "main.h"
-
-
-int main(void)
+int _islower(int c)
 {
-    int r;
-
-        r = _islower('H');
-        _putchar(r + '0');
-        r = _islower('o');
-        _putchar(r + '0');
-        r = _islower(108);
-        _putchar(r + '0');
-        _putchar('\n');
-        return (0);
+    if (c >= 'a' && c <= 'z')
+    {
+        return (1);
+    }
+    return (0);
 }
-
