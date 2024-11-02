3175986d136189bd1c0184605e9fadd1e395192f
turtle-nest <n78693107@gmail.com>
2024-11-02 13:33:07 +0100

    Update 1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,11 +1,13 @@
 int _putchar(char c);
 void print_alphabet(void);
 
-void _putchar(char c) {
+void _putchar(char c) 
+{
     write(1, &c, 1);
 }
 
-void print_alphabet(void) {
+void print_alphabet(void) 
+{
     char letter = 'a';
     while (letter <= 'z') {
         _putchar(letter);
@@ -14,7 +16,8 @@ void print_alphabet(void) {
     _putchar('\n');
 }
 
-int main(void) {
+int main(void) 
+{
     print_alphabet();
     return 0;
 }
