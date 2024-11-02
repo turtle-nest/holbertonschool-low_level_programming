56c87a84fb3d316cb7c12d38f1706003a392c3e9
turtle-nest <n78693107@gmail.com>
2024-11-02 16:01:47 +0000

    TASK 0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  *reset_to_98 - takes a pointer to an int
- * @*n: is a pointer 
+ * @n: n is an integer
  *
  * Description: as parameter and updates the value to to 98
  *
  * Return: Always 0.
  */
-void reset_to_98(int *n) 
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
