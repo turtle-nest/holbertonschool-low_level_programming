4f8895fba5177b5c5290d9e5b6c69bbad1aecf93
turtle-nest <n78693107@gmail.com>
2024-11-02 19:04:39 +0000

    REVIEW

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,27 +1,27 @@
 #include "main.h"
 /**
- * main - checks the instructions
- * @void: type int
+ * jack_bauer - checks the instructions
+ * @void: void
  *
- * Description:
+ * Description: 24h chrono
 (* section header: Section description)*
  * Return: Always return 0.
  */
 
 void jack_bauer(void)
 {
-    int hour, minute;
+int hour, minute;
 
-    for (hour = 0; hour < 24; hour++)
-    {
-        for (minute = 0; minute < 60; minute++)
-        {
-            _putchar((hour / 10) + '0');
-            _putchar((hour % 10) + '0');
-            _putchar(':');
-            _putchar((minute / 10) + '0');
-            _putchar((minute % 10) + '0');
-            _putchar('\n');
-        }
-    }
+for (hour = 0; hour < 24; hour++)
+{
+for (minute = 0; minute < 60; minute++)
+{
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
+_putchar(':');
+_putchar((minute / 10) + '0');
+_putchar((minute % 10) + '0');
+_putchar('\n');
+}
+}
 }
