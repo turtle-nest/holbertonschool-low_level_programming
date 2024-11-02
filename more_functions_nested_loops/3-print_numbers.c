89a423f15a1615688dfe9ce56a3548fb210bba11
turtle-nest <n78693107@gmail.com>
2024-11-02 14:25:25 +0000

    TASK 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_numbers - prints the numbers
+ *@void: parameter void
+ *
+ * Description: a function that prints the numbers, from 0 to 9, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	char numbers[] = "0123456789\n";
+	char *ptr = numbers;
+
+	while (*ptr)
+	{
+	_putchar(*ptr++);
+	}
+}
