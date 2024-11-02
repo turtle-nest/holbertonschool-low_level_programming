015289b1e08b35327ebeabf940dab5b1d670ab9e
turtle-nest <n78693107@gmail.com>
2024-11-02 15:07:19 +0000

    TASK 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *print_triangle - prints a triangle
+ *@size: the size of the triangle
+ *
+ * Description:  a function that prints a triangle
+ *
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size > 0)
+	{
+	for (i = 1; i <= size; i++)
+	{
+	for (j = size - i; j > 0; j--)
+	{
+	_putchar(' ');
+	}
+	for (k = 0; k < i; k++)
+	{
+	_putchar('#');
+	}
+	_putchar('\n');
+	}
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
