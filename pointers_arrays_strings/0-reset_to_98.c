7d7a64288445a6227055cfcd248a27b4cebbcfb3
turtle-nest <n78693107@gmail.com>
2024-11-02 15:56:26 +0000

    TASK 0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -7,14 +7,13 @@
  *
  * Return: Always 0.
  */
-void reset_to_98(int *n) {
-    *n = 98;
-}
-
-int main() {
-    int valeur = 0;
-    reset_to_98(&valeur);
-    _putchar(valeur + '0');
-    _putchar('\n');
-    return 0;
+void reset_to_98(int *n) 
+{
+	*n = 98;
+	int valeur = 0;
+	
+	reset_to_98(&valeur);
+	_putchar(valeur + '0');
+	_putchar('\n');
+	return 0;
 }
