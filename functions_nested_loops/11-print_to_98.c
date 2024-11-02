4ee3aa559d51bdfe342c704be6f85c6a24a0aa5f
turtle-nest <n78693107@gmail.com>
2024-11-02 20:13:41 +0100

    Update 11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -4,42 +4,34 @@
  * @n: type int
  *
  * Description: prints 98
-(* section header: Section description)*
  * Return: Always return 0.
  */
-
 void print_to_98(int n)
 {
 while (n != 98)
 {
 char num[4];
 int temp = n, i = 0;
-
 if (n < 0)
 {
 _putchar('-');
 temp = -temp;
 }
-
 while (temp > 0)
 {
 num[i++] = (temp % 10) + '0';
 temp /= 10;
 }
-
 while (i > 0)
 {
 _putchar(num[--i]);
 }
-
 if (n == 0)
 {
 _putchar('0');
 }
-
 _putchar(',');
 _putchar(' ');
-
 if (n < 98)
 {
 n++;
@@ -49,7 +41,6 @@ else
 n--;
 }
 }
-
 _putchar('9');
 _putchar('8');
 _putchar('\n');
