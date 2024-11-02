16636e95e4fafa9cdfb10d1d4809ed2c8c2112a1
turtle-nest <n78693107@gmail.com>
2024-11-02 16:18:09 +0000

    TASK 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,26 @@
 #include "main.h"
 /**
- * - 
- * @: 
+ *print_rev - prints a string, in reverse
+ *@s: is a string
  *
- * Description: 
+ * Description: prints a string, in reverse
  *
- * Return: 
+ * Return: Always 0.
  */
+void print_rev(char *s)
+{
+	int length = 0;
 
+	while (s[length])
+	{
+	length++;
+	}
+
+	while (length > 0)
+	{
+	_putchar(s[length - 1]);
+	length--;
+	}
+
+	_putchar('\n');
+}
