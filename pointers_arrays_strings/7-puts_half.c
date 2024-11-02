fe7f263566f4bad2eb9acfd959f4a68e49978992
turtle-nest <n78693107@gmail.com>
2024-11-02 16:31:37 +0000

    TASK 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,10 +1,36 @@
 #include "main.h"
 /**
- * - 
- * @: 
+ *puts_half - prints half of a string
+ *@str: is a string
  *
- * Description: 
+ * Description: prints half of a string
  *
- * Return: 
+ * Return: 0.
  */
+void puts_half(char *str)
+{
+	int length = 0;
+	int start;
 
+	while (str[length])
+	{
+	length++;
+	}
+
+	if (length % 2 == 0)
+	{
+	start = length / 2;
+	}
+	else
+	{
+	start = (length - 1) / 2 + 1;
+	}
+
+	while (str[start])
+	{
+	_putchar(str[start]);
+	start++;
+	}
+
+	_putchar('\n');
+}
