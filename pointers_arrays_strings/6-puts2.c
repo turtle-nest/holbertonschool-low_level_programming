9f00ebbdf465417273f2e1016fd1e76521cae180
turtle-nest <n78693107@gmail.com>
2024-11-02 16:25:19 +0000

    TASK 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,10 +1,24 @@
 #include "main.h"
 /**
- * - 
- * @: 
+ *puts2 - prints every other character of a string
+ *@str: is a string
  *
- * Description: 
+ * Description: starting with the first character
  *
- * Return: 
+ * Return: 0.
  */
+void puts2(char *str)
+{
+	int i = 0;
 
+	while (str[i])
+	{
+	if (i % 2 == 0)
+	{
+	_putchar(str[i]);
+	}
+	i++;
+	}
+
+	_putchar('\n');
+}
