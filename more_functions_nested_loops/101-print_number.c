1d8149cac96f9647712e03007e536aabdee40665
turtle-nest <n78693107@gmail.com>
2024-11-02 15:19:50 +0000

    TASK 12

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -10,16 +10,23 @@
  */
 void print_number(int n) 
 {
-	if (n < 0) 
+	if (n == -2147483648) 
 	{
+	_putchar('-');
+	_putchar('2');
+	n = 147483648;
+	}
+	else if (n < 0)
+       	{
 	_putchar('-');
 	n = -n;
 	}
 
-	if (n / 10) 
+	if (n / 10)
 	{
 	print_number(n / 10);
 	}
 
 	_putchar((n % 10) + '0');
 }
+
