2a12bc69253fdfe5736e5f45313dc152748b4e3b
turtle-nest <n78693107@gmail.com>
2024-11-02 16:09:48 +0000

    TASK 2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *_strlen - returns the length of a string.
- *s@: s is a string 
+ *@s: s is a string 
  *
  * Description: returns the length of a string.
  *
@@ -17,5 +17,5 @@ int _strlen(char *s)
 	s++;
 	}
 
-	return length;
+	return (length);
 }
