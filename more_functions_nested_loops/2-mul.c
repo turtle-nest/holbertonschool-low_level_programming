765710878b8a6d62e175ffaaba2a6658a7a40660
turtle-nest <n78693107@gmail.com>
2024-11-02 14:19:30 +0000

    TASK 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *int mul: multiplies two integers
+ *@a: a integer to be multiplied
+ *@b: a integer to be multiplied
+ *
+ * Description: a function that multiplies two integers.
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
