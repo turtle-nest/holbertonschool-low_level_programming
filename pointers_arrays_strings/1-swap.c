cf81bf69b13b76caee4ae01ec7e6b1396ced80b3
turtle-nest <n78693107@gmail.com>
2024-11-02 16:05:26 +0000

    TASK 1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,10 +1,16 @@
 #include "main.h"
 /**
- * - 
- * @: 
+ *swap_int - swaps the values of two integers
+ * @a: a is an integer
+ * @b: b is an integer
  *
- * Description: 
+ * Description: swaps the values of two integers.
  *
- * Return: 
+ * Return: Always 0.
  */
-
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
