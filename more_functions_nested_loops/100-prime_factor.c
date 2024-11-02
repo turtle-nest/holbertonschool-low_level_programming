b16e2ef394bd8142fd54d8d37b01a314ee08b344
turtle-nest <n78693107@gmail.com>
2024-11-02 18:29:11 +0000

    TASK 11

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * - checks for uppercase character.
+ *@c: the character to be checked
+ *
+ * Description: a function that checks for uppercase character
+ *
+ * Return: 1 if the character is uppercase, 0 otherwise.
+ */
+#include <stdio.h>
+#include <math.h>
+
+long largest_prime_factor(long n) {
+    long max_prime = -1;
+
+    while (n % 2 == 0) {
+        max_prime = 2;
+        n /= 2;
+    }
+
+    for (long i = 3; i <= sqrt(n); i += 2) {
+        while (n % i == 0) {
+            max_prime = i;
+            n /= i;
+        }
+    }
+
+    if (n > 2) {
+        max_prime = n;
+    }
+
+    return max_prime;
+}
