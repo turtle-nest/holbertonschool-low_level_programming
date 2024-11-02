e24f55e099232b9f92546abe52b4f5928466e107
turtle-nest <n78693107@gmail.com>
2024-11-02 17:44:45 +0000

    TASK 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,10 +1,31 @@
 #include "main.h"
 /**
- * - 
- * @: 
+ * char *_strncpy - copies a string.
+ * @dest: a string
+ * @src: a string
+ * @n: an integer
  *
- * Description: 
+ * Description: copies a string.
  *
  * Return: Always 0.
  */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *original_dest = dest;
 
+	while (n-- > 0)
+	{
+	if (*src)
+	{
+	*dest = *src;
+	src++;
+	}
+	else
+	{
+	*dest = '\0';
+	}
+	dest++;
+	}
+
+	return (original_dest);
+}
