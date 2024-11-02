72b2b060cf8a4b9c430fe8b95398e57afdbd21f9
turtle-nest <n78693107@gmail.com>
2024-11-02 20:12:17 +0100

    Update 11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,56 +1,56 @@
 #include "main.h"
 /**
- * main - checks the instructions
- * @void: type int
+ * print_to_98 - checks the instructions
+ * @n: type int
  *
- * Description:
+ * Description: prints 98
 (* section header: Section description)*
  * Return: Always return 0.
  */
 
 void print_to_98(int n)
 {
-    while (n != 98)
-    {
-        char num[4];
-        int temp = n, i = 0;
+while (n != 98)
+{
+char num[4];
+int temp = n, i = 0;
 
-        if (n < 0)
-        {
-            _putchar('-');
-            temp = -temp;
-        }
+if (n < 0)
+{
+_putchar('-');
+temp = -temp;
+}
 
-        while (temp > 0)
-        {
-            num[i++] = (temp % 10) + '0';
-            temp /= 10;
-        }
+while (temp > 0)
+{
+num[i++] = (temp % 10) + '0';
+temp /= 10;
+}
 
-        while (i > 0)
-        {
-            _putchar(num[--i]);
-        }
+while (i > 0)
+{
+_putchar(num[--i]);
+}
 
-        if (n == 0)
-        {
-            _putchar('0');
-        }
+if (n == 0)
+{
+_putchar('0');
+}
 
-        _putchar(',');
-        _putchar(' ');
+_putchar(',');
+_putchar(' ');
 
-        if (n < 98)
-        {
-            n++;
-        }
-        else
-        {
-            n--;
-        }
-    }
+if (n < 98)
+{
+n++;
+}
+else
+{
+n--;
+}
+}
 
-    _putchar('9');
-    _putchar('8');
-    _putchar('\n');
+_putchar('9');
+_putchar('8');
+_putchar('\n');
 }
