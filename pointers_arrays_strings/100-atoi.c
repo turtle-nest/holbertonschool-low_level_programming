d3c7bd014db287cf024e99764a73f3c52d2e7746
turtle-nest <n78693107@gmail.com>
2024-11-02 16:51:16 +0000

    TASK 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,10 +1,39 @@
 #include "main.h"
 /**
- * - 
- * @: 
+ *_atoi - convert a string to an integer.
+ *@s: is a string
  *
- * Description: 
+ * Description: convert a string to an integer.
  *
- * Return: 
+ * Return: 0.
  */
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+	int started = 0;
 
+	while (*s)
+	{
+	if (*s == '-')
+	{
+	sign *= -1;
+	}
+	else if (*s == '+')
+	{
+
+        }
+	else if (*s >= '0' && *s <= '9')
+	{
+	started = 1;
+	num = num * 10 + (*s - '0');
+	}
+	else if (started)
+	{
+	break;
+	}
+	s++;
+	}
+
+	return (sign * num);
+}
