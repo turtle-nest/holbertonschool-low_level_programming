f4c99e14fb62fa8c5cab4453bc64e33d6058e347
turtle-nest <n78693107@gmail.com>
2024-11-02 18:37:18 +0000

    TASK 11

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -1,43 +1,49 @@
 #include "main.h"
 
 /**
- * - checks for uppercase character.
- *@c: the character to be checked
+ *main - checks the instructions
+ *@void: void parameter
  *
- * Description: a function that checks for uppercase character
+ * Description: finds & prints largest prime factor of 612852475143
  *
- * Return: 1 if the character is uppercase, 0 otherwise.
+ * Return: 0.
  */
 #include <stdio.h>
 #include <math.h>
 
-long largest_prime_factor(long n) {
-    long max_prime = -1;
-    long i;
+long largest_prime_factor(long n)
+{
+	long max_prime = -1;
+	long i;
 
-    while (n % 2 == 0) {
-        max_prime = 2;
-        n /= 2;
-    }
+	while (n % 2 == 0)
+	{
+	max_prime = 2;
+	n /= 2;
+	}
 
-    for (i = 3; i <= sqrt(n); i += 2) {
-        while (n % i == 0) {
-            max_prime = i;
-            n /= i;
-        }
-    }
+	for (i = 3; i <= sqrt(n); i += 2)
+	{
+	while (n % i == 0)
+	{
+	max_prime = i;
+	n /= i;
+	}
+	}
 
-    if (n > 2) {
-        max_prime = n;
-    }
+	if (n > 2)
+	{
+	max_prime = n;
+	}
 
-    return max_prime;
+	return (max_prime);
 }
 
-int main() {
-    long number = 612852475143;
-    long result = largest_prime_factor(number);
-    printf("%ld\n", result);
-    return 0;
+int main(void)
+{
+	long number = 612852475143;
+	long result = largest_prime_factor(number);
+	printf("%ld\n", result);
+	return (0);
 }
 
