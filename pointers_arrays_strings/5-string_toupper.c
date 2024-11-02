d748f6f28356b6e15b980270c663f479de43fd8e
turtle-nest <n78693107@gmail.com>
2024-11-02 17:56:49 +0000

    TASK 5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- **string_toupper - changes all lowercase letters of a string to uppercase.
+ *string_toupper - changes all lowercase letters of a string to uppercase.
  * @char: a string
  *
  * Description: changes all lowercase letters of a string to uppercase.
