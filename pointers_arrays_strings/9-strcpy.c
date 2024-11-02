6b5380ab00c50f40c04673e43d77a097db476e1c
turtle-nest <n78693107@gmail.com>
2024-11-02 16:44:58 +0000

    TASK 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,10 +1,22 @@
 #include "main.h"
 /**
- * - 
- * @: 
+ **_strcpy - copies the string pointed to by src
+ *@dest: is a string
+ *@src: is a string
  *
- * Description: 
+ * Description: copies the string pointed to by src
  *
- * Return: 
+ * Return: 0.
  */
+char *_strcpy(char *dest, char *src)
+{
+	char *ptr = dest;
 
+	while (*src)
+	{
+	*ptr++ = *src++;
+	}
+	*ptr = '\0';
+
+	return dest;
+}
