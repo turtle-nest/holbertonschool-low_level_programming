361d4c9e3501c53619880777a5d1b406ee3e084e
turtle-nest <n78693107@gmail.com>
2024-11-02 18:57:56 +0000

    REVIEW

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,28 +1,28 @@
 #include "main.h"
 /**
- * main - checks the instructions
- * @void: type int
+ * print_sign - checks the instructions
+ * @n: type int
  *
- * Description:
+ * Description: prints the sign of a number
 (* section header: Section description)*
  * Return: Always return 0.
  */
 
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        _putchar('+');
-        return (1);
-    }
-    else if (n == 0)
-    {
-        _putchar('0');
-        return (0);
-    }
-    else
-    {
-        _putchar('-');
-        return (-1);
-    }
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
 }
