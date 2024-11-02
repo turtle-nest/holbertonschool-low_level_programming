8e2f9941237ab2859507cc57fd334f7b5645cf3e
turtle-nest <n78693107@gmail.com>
2024-11-02 18:21:01 +0000

    TASK 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    for (int i = 1; i <= 100; i++) {
+	int i;
+
+    for (i = 1; i <= 100; i++) {
         if (i % 3 == 0 && i % 5 == 0) {
             printf("FizzBuzz");
         } else if (i % 3 == 0) {
