e8dae11f653816ab579410052a8b2ad2a3f03396
turtle-nest <n78693107@gmail.com>
2024-11-02 16:21:09 +0000

    TASK 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,26 @@
 #include "main.h"
 /**
- * - 
- * @: 
+ *rev_string - reverses a string.
+ *@s: is a string
  *
- * Description: 
+ * Description: reverses a string.
  *
- * Return: 
+ * Return: Always 0.
  */
+void rev_string(char *s)
+{
+	int length = 0;
+	char temp;
+	int i;
 
+	while (s[length]) {
+	length++;
+	}
+
+	for (i = 0; i < length / 2; i++)
+	{
+	temp = s[i];
+	s[i] = s[length - 1 - i];
+	s[length - 1 - i] = temp;
+	}
+}
