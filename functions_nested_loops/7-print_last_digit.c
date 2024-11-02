fa78a275756bad78a60a0408271911ebe627361f
turtle-nest <n78693107@gmail.com>
2024-11-02 19:01:57 +0000

    REVIEW

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
- * main - checks the instructions
- * @void: type int
+ * print_last_digit - checks the instructions
+ * @n: type int
  *
- * Description:
+ * Description: print last digit
 (* section header: Section description)*
  * Return: Always return 0.
  */
 
 int print_last_digit(int n)
 {
-    int last_digit = n % 10;
+int last_digit = n % 10;
 
-    if (last_digit < 0)
-    {
-        last_digit = -last_digit;
-    }
+if (last_digit < 0)
+{
+last_digit = -last_digit;
+}
 
-    _putchar(last_digit + '0');
+_putchar(last_digit + '0');
 
-    return (last_digit);
+return (last_digit);
 }
