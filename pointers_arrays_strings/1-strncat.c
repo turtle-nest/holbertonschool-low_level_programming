bdfc881e8dfe70b66a50854cbaeeccae6a2de67d
turtle-nest <n78693107@gmail.com>
2024-11-02 17:34:15 +0000

    TASK 1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,30 @@
 #include "main.h"
 /**
- * - 
- * @: 
+ *char *_strncat - concatenates two strings.
+ * @dest: is a string
+ * @src: a srting
+ * @n: is number of bytes
  *
- * Description: 
+ * Description: concatenates two strings.
  *
  * Return: Always 0.
  */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *original_dest = dest;
 
+	while (*dest)
+	{
+	dest++;
+	}
+
+	while (n-- > 0 && *src)
+	{
+	*dest = *src;
+	dest++;
+	src++;
+	}
+
+	*dest = '\0';
+	return (original_dest);
+}
