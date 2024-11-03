86886ee3d018033a586dccda6e0e97eae4b48d7a
turtle-nest <n78693107@gmail.com>
2024-11-03 15:19:33 +0100

    Update 100-times_table.c

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - checks the instructions
  * @void: none
@@ -7,20 +8,29 @@
  * (* section header: Section description)*
  * Return: none
  */
+void print_number(int n) {
+    if (n < 10) {
+        putchar(n + '0');
+    } else {
+        print_number(n / 10);
+        putchar(n % 10 + '0');
+    }
+}
+
 void print_times_table(int n) {
-int i;
-	
+	int i;
     if (n < 0 || n > 15) {
         return;
     }
 
     for (i = 0; i <= 10; i++) {
-        printf("%d", n * i);
+        int product = n * i;
+        print_number(product);
         if (i < 10) {
-            printf(", ");
+            write(1, ", ", 2);
         }
     }
-    printf("\n");
+    write(1, "\n", 1);
 }
 
 
