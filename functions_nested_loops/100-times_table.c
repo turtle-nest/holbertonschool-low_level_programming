0d44137ad341df8486dacb50bbe3df8c0429ab6b
turtle-nest <n78693107@gmail.com>
2024-11-03 14:16:12 +0000

    TASK 12

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,22 @@
+/**
+ * main - checks the instructions
+ * @void: none
+ *
+ * Description:  prints _putchar, followed by a new line.
+ * (* section header: Section description)*
+ * Return: none
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+#include <unistd.h>
+
+int main(void)
+{
+	char msg[] = "_putchar\n";
+
+	write(1, msg, sizeof(msg) - 1);
+	return (0);
+}
+
