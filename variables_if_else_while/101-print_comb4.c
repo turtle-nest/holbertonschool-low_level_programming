146d4bafa67d6d7144a5430e4a9db9bc6a71bbb8
turtle-nest <n78693107@gmail.com>
2024-11-03 11:34:54 +0100

    Update 101-print_comb4.c

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -1,31 +1,34 @@
+#include <stdio.h>
 /**
  * main - checks the instructions
- * @void: none
+ * @void: void.
  *
- * Description: prints all possible different combinations of three digits.
+ * Description: all possible different combinations of three digits.
 (* section header: Section description)*
- * Return: none
+ * Return: 0.
  */
-#include <stdio.h>
+int main(void)
+{
+int i, j, k;
 
-int main(void) {
-    int i, j, k;
-
-    for (i = 0; i < 8; i++) {
-        for (j = i + 1; j < 9; j++) {
-            for (k = j + 1; k < 10; k++) {
-                putchar(i + '0');
-                putchar(j + '0');
-                putchar(k + '0');
-                if (i < 7) {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-    putchar('\n');
-
-    return 0;
+for (i = 0; i < 8; i++)
+{
+for (j = i + 1; j < 9; j++)
+{
+for (k = j + 1; k < 10; k++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
+if (i < 7)
+{
+putchar(',');
+putchar(' ');
 }
+}
+}
+}
+putchar('\n');
 
+return (0);
+}
