c5773442a5fc73f7c15cb1a80c079b2321e4ec5d
turtle-nest <n78693107@gmail.com>
2024-11-03 15:11:27 +0100

    Update 101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,7 +1,15 @@
 #include <unistd.h>
-
-int main(void) {
-    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, sizeof(message) - 1);
-    return 1;
+/**
+ * main - checks the instructions
+ * @void: void.
+ *
+ * Description: checks the instructions
+ *
+ * Return: 1.
+ */
+int main(void) 
+{
+const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, message, sizeof(message) - 1);
+return (1);
 }
