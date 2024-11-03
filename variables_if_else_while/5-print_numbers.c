657b08e5efde3e54e88770901b6afdd13cbba860
turtle-nest <n78693107@gmail.com>
2024-11-03 11:23:48 +0100

    Update 5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -11,9 +11,9 @@
  */
 int main(void)
 {
-int number; 
-for (number = 0; number <= 9; number++) 
-putchar(number + '0'); 
-putchar('\n'); 
+int number;
+for (number = 0; number <= 9; number++)
+putchar(number + '0');
+putchar('\n');
 return (0);
 }
