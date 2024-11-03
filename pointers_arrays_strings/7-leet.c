8cb724e0d096ed5017e18deac742ed1de1d050ae
turtle-nest <n78693107@gmail.com>
2024-11-03 08:42:07 +0000

    TASK 7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,32 +1,35 @@
 #include "main.h"
 /**
- * - 
- * @: 
+ *leet - encodes a string into 1337.
+ * @str: is a string
  *
- * Description: 
+ * Description: encodes a string into 1337.
  *
- * Return: Always 0.
+ * Return: str.
  */
-char *leet(char *str) {
-    char leet_map[256] = {0};
-    int i;
+char *leet(char *str)
+{
+	char leet_map[256] = {0};
+	int i;
 
-    leet_map['a'] = '4';
-    leet_map['A'] = '4';
-    leet_map['e'] = '3';
-    leet_map['E'] = '3';
-    leet_map['o'] = '0';
-    leet_map['O'] = '0';
-    leet_map['t'] = '7';
-    leet_map['T'] = '7';
-    leet_map['l'] = '1';
-    leet_map['L'] = '1';
+	leet_map['a'] = '4';
+	leet_map['A'] = '4';
+	leet_map['e'] = '3';
+	leet_map['E'] = '3';
+	leet_map['o'] = '0';
+	leet_map['O'] = '0';
+	leet_map['t'] = '7';
+	leet_map['T'] = '7';
+	leet_map['l'] = '1';
+	leet_map['L'] = '1';
 
-    for (i = 0; str[i] != '\0'; i++) {
-        if (leet_map[(unsigned char)str[i]]) {
-            str[i] = leet_map[(unsigned char)str[i]];
-        }
-    }
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	if (leet_map[(unsigned char)str[i]])
+	{
+	str[i] = leet_map[(unsigned char)str[i]];
+	}
+	}
 
-    return str;
+	return (str);
 }
