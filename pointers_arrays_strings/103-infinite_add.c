b1929d8cc1015f891d99337d21dca008ff453221
turtle-nest <n78693107@gmail.com>
2024-11-03 09:20:54 +0000

    TASK 11

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -11,46 +11,33 @@
  * Return: 0 if result not stored in r, otherwise r.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r) {
-    int len1 = 0, len2 = 0, carry = 0, i, j, k, l;
+    int len1 = strlen(n1);
+    int len2 = strlen(n2);
+    int carry = 0;
+    int i = len1 - 1, j = len2 - 1, k = 0;
 
-    while (n1[len1] != '\0') len1++;
-    while (n2[len2] != '\0') len2++;
-
-    if (len1 > size_r || len2 > size_r || len1 + len2 + 1 > size_r) {
+    if (size_r <= (len1 > len2 ? len1 : len2) + 1) {
         return 0;
     }
 
-    r[len1 + len2] = '\0';
-    i = len1 - 1;
-    j = len2 - 1;
-    k = 0;
-
-    while (i >= 0 || j >= 0 || carry) {
-        int sum = carry;
-        if (i >= 0) sum += n1[i--] - '0';
-        if (j >= 0) sum += n2[j--] - '0';
+    while (i >= 0 || j >= 0 || carry > 0) {
+        int digit1 = (i >= 0) ? n1[i--] - '0' : 0;
+        int digit2 = (j >= 0) ? n2[j--] - '0' : 0;
+        int sum = digit1 + digit2 + carry;
 
         carry = sum / 10;
-        r[len1 + len2 - 1 - k++] = (sum % 10) + '0';
+        r[k++] = (sum % 10) + '0';
     }
 
-    if (k >= size_r) return 0;
-
-    if (carry) {
-        if (k + 1 >= size_r) return 0;
-        for (l = k; l > 0; l--) {
-            r[l] = r[l - 1];
-        }
-        r[0] = carry + '0';
-        k++;
+    if (k >= size_r) {
+        return 0;
     }
 
     r[k] = '\0';
-
-    for (l = 0; l < k / 2; l++) {
-        char temp = r[l];
-        r[l] = r[k - 1 - l];
-        r[k - 1 - l] = temp;
+    for (int start = 0, end = k - 1; start < end; start++, end--) {
+        char temp = r[start];
+        r[start] = r[end];
+        r[end] = temp;
     }
 
     return r;
