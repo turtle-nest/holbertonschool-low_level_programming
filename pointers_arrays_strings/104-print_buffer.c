58892deda1ddea63a7ee6c6deee46169910b96e0
turtle-nest <n78693107@gmail.com>
2024-11-03 10:41:23 +0100

    Update 104-print_buffer.c

--- pointers_arrays_strings/104-print_buffer.c
+++ pointers_arrays_strings/104-print_buffer.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  *print_buffer - prints a buffer.
  * @b: is a char
@@ -8,42 +9,53 @@
  *
  * Return: void.
  */
-#include <stdio.h>
+void print_buffer(char *b, int size)
+{
+int i, j;
 
-void print_buffer(char *b, int size) {
-    int i, j;
+if (size <= 0)
+{
+putchar('\n');
+return;
+}
 
-    if (size <= 0) {
-        putchar('\n');
-        return;
-    }
+for (i = 0; i < size; i += 10)
+{
+printf("%08x: ", i);
 
-    for (i = 0; i < size; i += 10) {
-        printf("%08x: ", i);
+for (j = 0; j < 10; j++)
+{
+if (i + j < size)
+{
+printf("%02x", (unsigned char)b[i + j]);
+}
+else
+{
+printf("  ");
+}
+if (j % 2 == 1)
+{
+printf(" ");
+}
+}
 
-        for (j = 0; j < 10; j++) {
-            if (i + j < size) {
-                printf("%02x", (unsigned char)b[i + j]);
-            } else {
-                printf("  ");
-            }
-            if (j % 2 == 1) {
-                printf(" ");
-            }
-        }
+for (j = 0; j < 10; j++)
+{
+if (i + j < size)
+{
+char c = b[i + j];
+if (c >= 32 && c <= 126)
+{
+putchar(c);
+}
+else
+{
+putchar('.');
+}
+}
+}
 
-        for (j = 0; j < 10; j++) {
-            if (i + j < size) {
-                char c = b[i + j];
-                if (c >= 32 && c <= 126) {
-                    putchar(c);
-                } else {
-                    putchar('.');
-                }
-            }
-        }
-
-        putchar('\n');
-    }
+putchar('\n');
+}
 }
 
