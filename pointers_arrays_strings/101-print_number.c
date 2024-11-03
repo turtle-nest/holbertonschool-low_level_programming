675a66604f7eb8a85cbfd9ad307a967eb7afa647
turtle-nest <n78693107@gmail.com>
2024-11-03 08:51:55 +0000

    TASK 8

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_number - prints an integer.
+ * @n: n is an integer
+ *
+ * Description: prints an integer.
+ *
+ * Return: void.
+ */
+void print_number(int n) {
+    if (n < 0) {
+        _putchar('-');
+        n = -n;
+    }
+
+    if (n / 10) {
+        print_number(n / 10);
+    }
+    _putchar((n % 10) + '0');
+}
