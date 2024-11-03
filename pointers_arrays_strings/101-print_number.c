347f0df8f48cce25d4de2f16cccde73b615ea644
turtle-nest <n78693107@gmail.com>
2024-11-03 08:55:10 +0000

    TASK 9

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -7,19 +7,25 @@
  *
  * Return: void.
  */
-void print_number(int n) {
-    if (n < 0) {
-        _putchar('-');
-        if (n == -2147483648) {
-            _putchar('2');
-            n = 147483648;
-        } else {
-            n = -n;
-        }
-    }
+void print_number(int n)
+{
+	if (n < 0)
+	{
+	_putchar('-');
+	if (n == -2147483648)
+	{
+	_putchar('2');
+	n = 147483648;
+	}
+	else
+	{
+	n = -n;
+	}
+	}
 
-    if (n / 10) {
-        print_number(n / 10);
-    }
-    _putchar((n % 10) + '0');
+	if (n / 10)
+	{
+	print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
 }
