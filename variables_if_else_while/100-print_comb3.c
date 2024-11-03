9000ebe72a4bce0f247a34242017556422061dd2
turtle-nest <n78693107@gmail.com>
2024-11-03 11:32:01 +0100

    Update 100-print_comb3.c

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * main - checks the instructions
  * @void: none
@@ -6,23 +7,24 @@
 (* section header: Section description)*
  * Return: none
  */
-#include <stdio.h>
+int main(void)
+{
+int i, j;
 
-int main(void) {
-    int i, j;
-
-    for (i = 0; i < 9; i++) {
-        for (j = i + 1; j < 10; j++) {
-            putchar(i + '0');
-            putchar(j + '0');
-            if (i < 8) {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
-
-    return 0;
+for (i = 0; i < 9; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
+if (i < 8)
+{
+putchar(',');
+putchar(' ');
 }
+}
+}
+putchar('\n');
 
+return (0);
+}
