4cb769fc89942103cec4556b52640e2382e763b5
turtle-nest <n78693107@gmail.com>
2024-11-03 08:39:04 +0000

    TASK 7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,6 +9,7 @@
  */
 char *leet(char *str) {
     char leet_map[256] = {0};
+    int i;
 
     leet_map['a'] = '4';
     leet_map['A'] = '4';
@@ -20,7 +21,6 @@ char *leet(char *str) {
     leet_map['T'] = '7';
     leet_map['l'] = '1';
     leet_map['L'] = '1';
-    int i;
 
     for (i = 0; str[i] != '\0'; i++) {
         if (leet_map[(unsigned char)str[i]]) {
