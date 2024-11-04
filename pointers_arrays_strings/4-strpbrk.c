957c7d11663e4ef17e6e5f154b1a5ce901e0a190
turtle-nest <n78693107@gmail.com>
2024-11-04 13:25:52 +0000

    TASK 4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,9 +1,30 @@
 #include "main.h"
 /**
- * - 
- * @: 
+ * _strpbrk - recherche un caractère dans une chaîne
+ * @s: chaîne de caractères à analyser
+ * @accept: chaîne de caractères acceptés
  *
- * Description: 
- *
- * Return: Always 0.
+ * Return: pointeur vers le premier caractère de s qui est dans accept,
+ *         ou NULL si aucun caractère n'est trouvé
  */
+char *_strpbrk(char *s, char *accept)
+{
+while (*s)
+{
+char *a = accept;
+
+while (*a) 
+{
+if (*s == *a)
+{
+return (s);
+}
+a++;
+}
+
+s++;
+}
+
+return (NULL);
+}
+
