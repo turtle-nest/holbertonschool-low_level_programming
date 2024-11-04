d643b808d7b9ee87706639be0032001fbe81fc4d
turtle-nest <n78693107@gmail.com>
2024-11-04 13:46:11 +0000

    TASK 6

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,9 +1,21 @@
 #include "main.h"
 /**
- * - 
- * @: 
+ * print_chessboard - imprime un échiquier
+ * @a: tableau 2D représentant l'échiquier
  *
- * Description: 
- *
- * Return: Always 0.
+ * Return: Rien
  */
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+_putchar(a[i][j]);
+}
+_putchar('\n');
+}
+}
+
