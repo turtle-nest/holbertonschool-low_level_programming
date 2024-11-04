aacdd7778b4b024e866fee38346a3ccaa4d7f8ac
turtle-nest <n78693107@gmail.com>
2024-11-04 13:02:21 +0000

    TASK 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,9 +1,27 @@
 #include "main.h"
 /**
- * - 
- * @: 
+ * _strchr - localise un caractère dans une chaîne
+ * @s: chaîne de caractères à parcourir
+ * @c: caractère à trouver
  *
- * Description: 
- *
- * Return: Always 0.
+ * Return: pointeur vers c dans s, ou NULL si non trouvé
  */
+char *_strchr(char *s, char c)
+{
+while (*s)
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+
+if (c == '\0')
+{
+return (s);
+}
+    
+return (NULL);
+}
+
