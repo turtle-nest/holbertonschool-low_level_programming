c31f85d670bab1ea1462809fcbdabba73baca1ce
turtle-nest <n78693107@gmail.com>
2024-11-04 13:57:00 +0000

    TASK 7

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,9 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * - 
- * @: 
+ * print_diagsums - imprime la somme des deux diagonales d'une matrice
+ * @a: pointeur vers la matrice
+ * @size: taille de la matrice
  *
- * Description: 
- *
- * Return: Always 0.
+ * Return: Rien
  */
+void print_diagsums(int *a, int size)
+{
+int sum1 = 0;
+int sum2 = 0;
+int i;
+
+for (i = 0; i < size; i++)
+{
+sum1 += *(a + (i * size + i)); // Diagonale principale
+sum2 += *(a + (i * size + (size - i - 1))); // Diagonale secondaire
+}
+printf("%d, %d\n", sum1, sum2);
+}
+
