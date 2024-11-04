eefcbed9e5adb912a938519526817a11a2c20d25
turtle-nest <n78693107@gmail.com>
2024-11-04 14:00:06 +0000

    TASK 7

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -15,8 +15,8 @@ int i;
 
 for (i = 0; i < size; i++)
 {
-sum1 += *(a + (i * size + i)); // Diagonale principale
-sum2 += *(a + (i * size + (size - i - 1))); // Diagonale secondaire
+sum1 += *(a + (i * size + i));
+sum2 += *(a + (i * size + (size - i - 1)));
 }
 printf("%d, %d\n", sum1, sum2);
 }
