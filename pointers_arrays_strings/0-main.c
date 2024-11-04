f66218019e0f0d8eff78547871625d60e7d5398a
turtle-nest <n78693107@gmail.com>
2024-11-04 11:54:54 +0000

    TASK 0

---
+++ pointers_arrays_strings/0-main.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * simple_print_buffer - imprime le tampon en hexadécimal
+ * @buffer: l'adresse de la mémoire à imprimer
+ * @size: la taille de la mémoire à imprimer
+ *
+ * Return: Rien.
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+    unsigned int i;
+
+    i = 0;
+    while (i < size)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+/**
+ * main - vérifie le code
+ *
+ * Return: Toujours 0.
+ */
+int main(void)
+{
+    char buffer[98] = {0x00};
+
+    simple_print_buffer(buffer, 98);
+    _memset(buffer, 0x01, 95);
+    printf("-------------------------------------------------\n");
+    simple_print_buffer(buffer, 98);
+    return (0);
+}
+
