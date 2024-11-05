616fb6e59f4c8bc92a46811d2a8fb79e9ca9adf2
turtle-nest <n78693107@gmail.com>
2024-11-05 14:03:23 +0000

    TASK 4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _pow_recursion - returns the value of x raised to the power of y.
+ * @x: the base
+ * @y: the power
+ *
+ * Description: a recursive function pow
+ *
+ * Return: -1 if y < 0, otherwise the result.
+ */
+int _pow_recursion(int x, int y);
+{
+int i;
+
+if (y < 0)
+	return(-1)
+for (i = 1; i <= y; i++)
+	return(i * _pow_recursion(x,i));
+}
+
