b2002c0c73d5ae7f705a65164e8cce90fe27d131
turtle-nest <n78693107@gmail.com>
2024-11-06 11:00:41 +0100

    Update 5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -7,20 +7,24 @@
  *
  * Return: -1 if n haven't got a natural square root, or the result
  */
-int _sqrt_helper(int n, int guess) {
-    if (guess * guess == n) {
-        return guess;
-    }
-    if (guess * guess > n) {
-        return -1;
-    }
-    return _sqrt_helper(n, guess + 1);
+int _sqrt_helper(int n, int guess)
+{
+if (guess * guess == n)
+{
+return guess;
+}
+if (guess * guess > n)
+{
+return (-1);
+}
+return (_sqrt_helper(n, guess + 1));
 }
 
-int _sqrt_recursion(int n) {
-    if (n < 0) {
-        return -1;
-    }
-    return _sqrt_helper(n, 1);
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
 }
-
+return (_sqrt_helper(n, 1));
+}
