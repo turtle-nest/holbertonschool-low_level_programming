7b4c76627f42e4a0c1c336c2e3a4bbebe0871077
turtle-nest <n78693107@gmail.com>
2024-11-06 18:54:49 +0100

    TASK 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - prints its name
- * @:
+ * main - multiplication
  *
- * Description:
+ * Description: program that multiplies two numbers.
  *
- * Return: Always 0.
+ * Return: 1 if error, otherwise 0.
  */
+int main(int argc, char *argv[])
+{
+    int num1, num2, result;
 
+    if (argc != 3)
+    {
+(printf("Error\n"));
+return (1);
+    }
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
+    
+    result = num1 * num2;
+
+    printf("%d\n", result);
+
+    return (0);
+}
--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,10 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 /**
- * main - prints its name
- * @:
+ * main - addition
+ * @argc: argument count
+ * @argv: argument vector
  *
- * Description:
+ * Description: program that adds two positives numbers.
  *
- * Return: Always 0.
+ * Return: Error if is not digit numbers otherwise 0.
  */
+int main(int argc, char *argv[])
+{
+    int i, j, sum = 0;
 
+       if (argc == 1)
+       {
+        printf("0\n");
+        return (0);
+       }
+        for (i = 1; i < argc; i++)
+        {
+            for (j = 1; argv[i][j] != '\0'; j++)
+            {
+                if (!isdigit(argv[i][j]))
+                {
+                    printf("Error\n");
+                    return (1);
+                }
+            }
+
+            sum += atoi(argv[i]);
+        }
+
+        printf("%d\n", sum);
+
+        return (0);
+    }
