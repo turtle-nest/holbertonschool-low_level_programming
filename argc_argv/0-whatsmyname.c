6a5bc09e2a0f0220fe3da3583d94b1ee48b019c2
turtle-nest <n78693107@gmail.com>
2024-11-06 09:05:02 +0000

    TASK 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,21 +1,15 @@
-#include "main.h"
+#include <stdio.h>
+
 /**
- * main - prints its name
+ * main - prints its name, followed by a new line.
  * @argc: argument count
  * @argv: argument vector
  *
  * Return: Always 0.
  */
-int main(int argc, char *argv[])
-{
-int i;
-(void)argc;
-
-for (i = 0; argv[0][i] != '\0'; i++)
-{
-_putchar(argv[0][i]);
-}
-_putchar('\n');
-return (0);
+int main(int argc, char *argv[]) {
+    (void)argc;
+    printf("%s\n", argv[0]);
+    return 0;
 }
 
