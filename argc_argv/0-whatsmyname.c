c6e9aa29f571012cb80664314adc40f887bc5379
turtle-nest <n78693107@gmail.com>
2024-11-06 09:14:00 +0000

    TASK 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - prints its name, followed by a new line.
  * @argc: argument count
@@ -7,9 +6,10 @@
  *
  * Return: Always 0.
  */
-int main(int argc, char *argv[]) {
-    (void)argc;
-    printf("%s\n", argv[0]);
-    return 0;
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
 }
 
