570064ff14f7bc4a611eb2f4dde15e200f8461ab
turtle-nest <n78693107@gmail.com>
2024-11-06 17:41:18 +0100

    TASK 2

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 /**
  * main - prints all arguments it receives
- * @:
- * @:
+ * @argc: argument count
+ * @argv: argument vector
  *
  * Description: prints all arguments it receives
  *
  * Return: Always 0.
  */
+int main(int argc, char *argv[])
+{
+int i;
+(void)argc;
 
+for (i = 0; i < argc; i++)
+printf("%s\n", argv[i]);
+return (0);
+}
