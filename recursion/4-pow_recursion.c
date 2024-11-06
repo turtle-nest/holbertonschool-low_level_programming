ce36189d58ef35164dabfcdd4c235fc6199d50fc
turtle-nest <n78693107@gmail.com>
2024-11-06 10:52:27 +0100

    Update 4-pow_recursion.c

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -8,12 +8,11 @@
  *
  * Return: -1 if y < 0, otherwise the result.
  */
-int _pow_recursion(int x, int y)
+int _pow_recursion (int x, int y)
 {
 if (y < 0)
 	return(-1);
 if (y == 0)
-	return(1);
-return(x * _pow_recursion(x,y-1));
+	return (1);
+return(x * _pow_recursion (x, y - 1));
 }
-
