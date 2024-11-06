a73e33f758ab8cbd3a1b3d39ea148a5367f8a9f0
turtle-nest <n78693107@gmail.com>
2024-11-06 09:03:03 +0000

    TASK 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -8,13 +8,14 @@
  */
 int main(int argc, char *argv[])
 {
+int i;
 (void)argc;
 
-for (int i = 0; argv[0][i] != '\0'; i++)
+for (i = 0; argv[0][i] != '\0'; i++)
 {
 _putchar(argv[0][i]);
 }
 _putchar('\n');
-return 0;
+return (0);
 }
 
