27a2668554e7c715c5008e85a6ed8aaabb4edcbe
turtle-nest <n78693107@gmail.com>
2024-11-06 10:45:43 +0100

    Update 1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -2,16 +2,16 @@
 /**
  * _print_rev_recursion - prints a string in reverse
  * @s: the string to print
- * 
+ *
  * Description: prints a string in reverse.
- * 
+ *
  * Return: function type void.
  */
 void _print_rev_recursion(char *s)
 {
-    if (*s == '\0')
-        return;
+if (*s == '\0')
+return;
 
-    _print_rev_recursion(s + 1);
-    _putchar(*s);
+_print_rev_recursion(s + 1);
+_putchar(*s);
 }
