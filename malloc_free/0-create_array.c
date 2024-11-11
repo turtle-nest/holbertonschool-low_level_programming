92556bf0fb04b619d455e68a68cafcce6edd5eaa
turtle-nest <n78693107@gmail.com>
2024-11-11 13:18:26 +0100

    TASK 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *create_array - creates an array of chars, initializes with a specific char
+ * @size: size of the array
+ * @c: a char to initialize
+ *
+ * Return: a pointer to the array, or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+char *arr = (char *)malloc(size * sizeof(char));
+unsigned int i;
+
+if (arr == NULL)
+return (NULL);
+
+for (i = 0; i < size; i++)
+{
+arr[i] = c;
+}
+
+return (arr);
+}
