77fda13e7d698e01369918ad0144270347a58876
turtle-nest <n78693107@gmail.com>
2024-11-14 14:35:22 +0100

    TASK 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: structure dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ *
+ * Return: Nothing.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
+{
+d->name = name;
+d->age =age;
+d->owner = owner;
+}
+}
