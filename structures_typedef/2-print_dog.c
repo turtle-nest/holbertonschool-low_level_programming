ba60d8323dd7681cf5ecb60e85c9111229a7e77e
turtle-nest <n78693107@gmail.com>
2024-11-14 15:09:17 +0100

    TASK 2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - prints a struct dog
+ * @d: structure dog
+ *
+ * Return: Nothing.
+ */
+void print_dog(struct dog *d)
+{
+if (d->name == NULL)
+{
+printf("Name: (nil)");
+}
+
+if (d->age == 0 || d->owner == NULL)
+{
+printf("(nil)");
+}
+
+else
+{
+printf("Name : %s\n", d->name);
+printf("Age : %f\n", d->age);
+printf("Owner : %s\n", d->owner);
+}
+}
