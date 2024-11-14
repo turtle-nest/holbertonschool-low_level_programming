ae7feeef5574ea2e68950d8be255679a5dbf3e34
turtle-nest <n78693107@gmail.com>
2024-11-14 14:38:25 +0100

    TASK 1

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,4 +1,5 @@
-#include "main.h"
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * dog - define a structure dog
@@ -10,6 +11,9 @@
  */
 struct dog
 {
-char *name, *owner;
+char *name;
 float age;
-}d;
+char *owner;
+};
+
+#endif
