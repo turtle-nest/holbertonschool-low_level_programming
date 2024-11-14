76ab77d2d625de7f68db9da361ef2e1da2a7b34c
turtle-nest <n78693107@gmail.com>
2024-11-14 15:16:22 +0100

    TASK 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,12 +10,14 @@
  */
 void print_dog(struct dog *d)
 {
-if (d->name == NULL || d->age == 0 || d->owner == NULL)
-{
+if (d->name == NULL)
 printf("Name : (nil)");
+
+if (d->age == 0)
 printf("Age : (nil)");
+
+if (d->owner == NULL)
 printf("Owner : (nil)");
-}
 
 else
 {
