1b61be77e71653a537b781a934c09ad3c6cbce61
turtle-nest <n78693107@gmail.com>
2024-11-14 10:49:58 +0100

    TASK 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * dog - define a structure dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ *
+ * Description: datas about a dog
+ */
+struct dog
+{
+char *name, *owner;
+float age;
+}d;
