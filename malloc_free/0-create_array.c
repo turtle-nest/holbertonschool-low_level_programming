feee57b9855d0af83d7c452299e9978d327c9b1e
turtle-nest <n78693107@gmail.com>
2024-11-15 13:10:55 +0100

    TASK REVIEW

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -9,9 +9,13 @@
  */
 char *create_array(unsigned int size, char c)
 {
-char *arr = (char *)malloc(size * sizeof(char));
+char *arr;
 unsigned int i;
 
+if (size == 0)
+return (NULL);
+
+arr = (char *)malloc(size * sizeof(char));
 if (arr == NULL)
 return (NULL);
 
