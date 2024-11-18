57ce388f8f75c15f93434cab9f1abe08a546980f
turtle-nest <n78693107@gmail.com>
2024-11-18 14:02:15 +0100

    TASK 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: The name to print
+ * @f: a pointer to a print function
+ * @char: char parameter of the pointed function
+ *
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+if (name && f)
+f(name);
+}
