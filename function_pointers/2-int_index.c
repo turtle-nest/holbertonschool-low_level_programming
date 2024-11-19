c3fc1242456bf8545d8cba1d80e7d45c6af2dcb2
turtle-nest <n78693107@gmail.com>
2024-11-19 09:55:43 +0100

    TASK 2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -16,19 +16,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 int i;
 int count = 0;
 
-if (array && cmp)
-{
+if (!array || !cmp)
+return (-1);
+
 if (size <= 0)
 return (-1);
 
 for (i = 0; i < size; i++)
 {
 if (cmp(array[i]))
-{
 count++;
 }
-}
 return (count);
-}
-return (-1);
 }
