d9943f3fa14b4d38244931967052161a562e7685
turtle-nest <n78693107@gmail.com>
2024-11-19 09:42:56 +0100

    TASK 2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,34 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: an array of integers
+ * @size: the number of elements in the array array
+ * @cmp: a pointer to the function to be used to compare values
+ *
+ * Return: returns the index of the first element 
+ * for which the cmp function does not return 0.
+ * If no element matches, return -1.
+ * If size <= 0, return -1.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+int count = 0;
+
+if (array && cmp)
+{
+if (size <= 0)
+return (-1);
+
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]))
+{
+count++;
+}
+}
+return (count);
+}
+return (-1);
+}
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,7 +1,6 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-#include <stdlib.h>
-#include <stdio.h>
+#include <stddef.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
