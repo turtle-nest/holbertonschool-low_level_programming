53354a70ab9d0bbf40c265bc3b2b08a66b70a250
turtle-nest <n78693107@gmail.com>
2024-11-20 18:46:35 +0100

    TASK 3

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - selects the correct function
+ * to perform the operation asked
+ * @s: s is the operator passed as argument
+ *
+ * Return: A pointer to the function that corresponds
+ * to the operator given as a parameter.
+ * If s does not match any of the 5 expected operators
+ * return NULL.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = 
+{
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+
+while (ops[i].op)
+{
+if (*(ops[i].op) == *s && s[1] == '\0')
+{
+return (ops[i].f);
+}
+i++;
+}
+return (NULL);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,70 @@
+#include "3-calc.h"
+
+/**
+ * add - add two integers
+ * @a: an integer
+ * @b: another integer
+ *
+ * Return: the result.
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * sub - substracts two integers
+ * @a: an integer
+ * @b: another integer
+ *
+ * Return: the result.
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * mul - multiplies two integers
+ * @a: an integer
+ * @b: another integer
+ *
+ * Return: the result.
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * div - divides two integers
+ * @a: an integer
+ * @b: another integer
+ *
+ * Return: the result.
+ */
+int op_div(int a, int b)
+{
+if (b == 0)
+{
+return (0);
+}
+return (a / b);
+}
+
+/**
+ * mod - returns the remainder of the division 
+ * of two integers
+ * @a: an integer
+ * @b: another integer
+ *
+ * Return: the result.
+ */
+int op_mod(int a, int b)
+{
+if (b == 0)
+{
+return (0);
+}
+return (a % b);
+}
