c226e01fcf2eb9d63440abf06d9cdd467e96b973
turtle-nest <n78693107@gmail.com>
2024-11-21 11:21:54 +0100

    end of the TASK 1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers with a separator between them
+ * @separator: separator to be used
+ * @n: number of integers
+ *
+ * Return: None.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list args;
+unsigned int i;
+int number;
+
+if (separator == NULL)
+return;
+
+va_start (args, n);
+
+for (i = 0; i < n; i++)
+{
+number = va_arg(args, int);
+printf("%d", number);
+
+if (i < (n - 1))
+printf("%s", separator);
+}
+
+va_end(args);
+printf("\n");
+}
