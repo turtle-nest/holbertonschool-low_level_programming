15d910815f2a0764b197d652b83c9b4c7a904391
turtle-nest <n78693107@gmail.com>
2024-11-21 09:46:51 +0100

    end of the TASK 0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - adds all the parameters
+ * @n: number of parameters
+ *
+ * Return: The result, or 0 if n is zero.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+    va_list args;
+    unsigned int i, sum;
+
+    if (n == 0)
+    {
+        return (0);
+    }
+
+    va_start (args, n);
+
+    sum = 0;
+    for (i = 0; i < n; i++)
+    {
+        sum += va_arg (args, int);
+    }
+
+    va_end (args);
+    return (sum);
+}
