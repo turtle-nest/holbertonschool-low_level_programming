fcd61efddb572d071a74d17b149527d15adb966f
turtle-nest <n78693107@gmail.com>
2024-11-21 11:39:52 +0100

    end TASK 2

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings with a separator between them
+ * @separator: separator to be used
+ * @n: is the number of strings passed to the function
+ *
+ * Return: None.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+va_list args;
+unsigned int i;
+char *string;
+
+if (separator == NULL)
+return;
+
+va_start (args, n);
+
+for (i = 0; i < n; i++)
+{
+string = va_arg(args, char *);
+if ( string == NULL)
+{
+printf("(nil)");
+}
+
+printf("%s", string);
+
+if (i < (n - 1))
+printf("%s", separator);
+}
+
+va_end(args);
+printf("\n");
+}
