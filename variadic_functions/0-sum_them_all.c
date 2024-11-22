e33b056f672ae369dc0d5de794a8b8738113e5e5
turtle-nest <n78693107@gmail.com>
2024-11-22 17:59:12 +0100

    review all the tasks

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -8,22 +8,22 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i, sum;
+va_list args;
+unsigned int i, sum;
 
-    if (n == 0)
-    {
-        return (0);
-    }
+if (n == 0)
+{
+return (0);
+}
 
-    va_start (args, n);
+va_start(args, n);
 
-    sum = 0;
-    for (i = 0; i < n; i++)
-    {
-        sum += va_arg (args, int);
-    }
+sum = 0;
+for (i = 0; i < n; i++)
+{
+sum += va_arg(args, int);
+}
 
-    va_end (args);
-    return (sum);
+va_end(args);
+return (sum);
 }
