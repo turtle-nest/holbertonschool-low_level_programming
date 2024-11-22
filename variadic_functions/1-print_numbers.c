637bad4b61d2a5ae6fe30cad3d0d468e093f1843
turtle-nest <n78693107@gmail.com>
2024-11-22 18:23:11 +0100

    review all the tasks

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -11,7 +11,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 va_list args;
 unsigned int i;
-int number;
 
 if (separator == NULL)
 return;
@@ -20,10 +19,9 @@ va_start (args, n);
 
 for (i = 0; i < n; i++)
 {
-number = va_arg(args, int);
-printf("%d", number);
+printf("%d", va_arg(args, int));
 
-if (separator && i < (n - 1))
+if (separator != NULL && i != n - 1)
 printf("%s", separator);
 }
 
