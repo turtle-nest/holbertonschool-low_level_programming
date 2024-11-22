8304dda8cd8cb3ea3383100dca5c0b3cb2d4ca37
turtle-nest <n78693107@gmail.com>
2024-11-22 18:31:48 +0100

    review all the tasks

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,6 +3,7 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct printer - Struct printer
@@ -16,8 +17,10 @@ char type;
 void (*func)(va_list);
 } printer_t;
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+char *convert_to_string(int num);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
