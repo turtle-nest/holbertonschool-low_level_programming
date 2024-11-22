f4f99b6b918e094028a2c47c8f2507fde8dde9ef
turtle-nest <n78693107@gmail.com>
2024-11-22 18:09:47 +0100

    review all the tasks

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -21,4 +21,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-#endif 
+#endif
