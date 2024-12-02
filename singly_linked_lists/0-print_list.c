63b5028f2adad24e5bc9c9b9fdcb30c00d340dba
turtle-nest <n78693107@gmail.com>
2024-12-02 17:48:11 +0100

    Add the README and lists.h and complete task 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: a pointer to an element of the list
+ *
+ * Return: the number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	int nodes = 0;
+
+		while(h != NULL)
+		{
+			if (h->str == NULL)
+				printf("[0] (nil)\n");
+			else
+				printf("[%u] %s\n", h->len, h->str);
+
+			h = h->next;
+			nodes++;
+		}
+
+	return (nodes);
+}
