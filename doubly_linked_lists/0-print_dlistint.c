031c75a1434f6ead6948559cc2f25cca4227f615
turtle-nest <n78693107@gmail.com>
2024-12-04 14:16:18 +0100

    Add task 0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a list_t list
+ * @h: a pointer to an element of the list
+ *
+ * Return: the number of nodes.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int nodes = 0;
+
+		while(h != NULL)
+		{
+			printf("%d\n", h->n);
+
+			h = h->next;
+			nodes++;
+		}
+
+	return (nodes);
+}
