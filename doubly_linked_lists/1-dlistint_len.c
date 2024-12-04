d8872a3d50a31c8f89391b9bc91f178c5ae3a5b3
turtle-nest <n78693107@gmail.com>
2024-12-04 14:28:23 +0100

    Add task 1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a list_t list
+ * @h: a pointer to an element of the list
+ *
+ * Return: the number of nodes.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+    size_t len = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+
+    return (len);
+}
