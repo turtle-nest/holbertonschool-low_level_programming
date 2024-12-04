5967411abcef3fac7f8902336c0c17e45642cf58
turtle-nest <n78693107@gmail.com>
2024-12-04 15:14:57 +0100

    Add task 4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint- frees a dlistint_t list
+ * @head: a pointer to the head of the list
+ *
+ * Return: None.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+ 	while (head != NULL) 
+	{
+        temp = head;
+        head = head->next;
+        free(temp);
+	}
+}
