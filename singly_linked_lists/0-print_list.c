92f030de01668416ead57f5a8f8a0cf739d955f2
turtle-nest <n78693107@gmail.com>
2024-12-05 09:48:29 +0100

    Correct task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,16 +10,16 @@ size_t print_list(const list_t *h)
 {
 	int nodes = 0;
 
-		while(h != NULL)
-		{
-			if (h->str == NULL)
-				printf("[0] (nil)\n");
-			else
-				printf("[%u] %s\n", h->len, h->str);
+	while(h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
 
-			h = h->next;
-			nodes++;
-		}
+		h = h->next;
+		nodes++;
+	}
 
 	return (nodes);
 }
