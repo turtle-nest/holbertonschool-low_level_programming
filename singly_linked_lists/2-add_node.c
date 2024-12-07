5fee2450b6dbfa93d7fecc68b2cc640321349ebe
turtle-nest <n78693107@gmail.com>
2024-12-07 12:15:10 +0100

    Correction

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,6 +1,22 @@
 #include "lists.h"
 
 /**
+ * string_length - Calculates the length of a string.
+ * @str: The string whose length to calculate.
+ * 
+ * Return: The length of the string.
+ */
+int string_length(const char *str)
+{
+    int len = 0;
+
+    while (str[len] != '\0')
+        len++;
+
+    return len;
+}
+
+/**
  * add_node - adds a new node at the beginning of a list
  * @head: a double pointer to an element of the list
  * @str: a string
@@ -25,6 +41,11 @@ list_t *add_node(list_t **head, const char *str)
 
 	newNode->next = *head;
 	*head = newNode;
+
+ 	_putchar(string_length(newNode->str) + '0');
+    _putchar('\n');
+    _putchar(newNode->str);
+
 
 	return (newNode);
 }
