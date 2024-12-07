8f7ec5e5c3a4b64dba7b2c2e071a3b784e7c54e9
turtle-nest <n78693107@gmail.com>
2024-12-07 12:26:14 +0100

    Correction

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,56 +1,44 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * add_node_end - adds a new node at the end of a list
- * @str: a string
- *
- * Return: the address of the new element,
- * or NULL if it failed.
- */
-list_t *create_node(const char *str)
-{
-	list_t *new_node = malloc(sizeof(list_t));
-
-	if (!new_node)
-		exit(1);
-
-	new_node->str = strdup(str);
-	new_node->next = NULL;
-
-	return (new_node);
-}
-
-/**
- * add_node_end - adds a new node at the end of a list
- * @head: a double pointer to an element of the list
- * @str: a string
- *
- * Return: the address of the new element,
- * or NULL if it failed.
+ * add_node_end - Adds a new node at the end of a list_t list.
+ * @head: A pointer to the head of the list.
+ * @str: The string to be duplicated and added to the new node.
+ * 
+ * Return: The address of the new element, or NULL if it failed.
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node = create_node(str);
-	list_t *temp;
-	unsigned int len = 0;
+    list_t *new_node, *temp;
 
-	if (*head == NULL)
-	{
-		*head = new_node;
-		return(new_node);
-	}
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return (NULL);
 
-	temp = *head;
-	while (temp->next != NULL)
-	{
-		temp = temp->next;
-	}
+    new_node->str = strdup(str);
+    if (new_node->str == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
 
-	while (str[len] != '\0')
-        len++;
+    new_node->next = NULL;
 
-	new_node->len = len;
-	temp->next = new_node;
+    if (*head == NULL)
+    {
+        *head = new_node;
+    }
+    else
+    {
+        temp = *head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
 
-	return (new_node);
+    return (new_node);
 }
