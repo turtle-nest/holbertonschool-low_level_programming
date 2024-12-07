843bc02496935f88799f4ff9111b1cbdebd905eb
turtle-nest <n78693107@gmail.com>
2024-12-07 13:11:18 +0100

    Correction

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,45 +12,43 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *temp;
-	unsigned int i;
+    dlistint_t *new_node, *temp = *h;
+    unsigned int i = 0;
 
-	new_node = add_dnodeint(h, n);
+    if (!h)
+        return (NULL);
 
-	if (idx == 0)
-	{
-		new_node->next = *h;
-		if (*h != NULL)
-		{
-			(*h)->prev = new_node;
-		}
-		*h = new_node;
-		return (NULL);
-	}
+    if (idx == 0)
+        return (add_dnodeint(h, n));
 
-	temp = *h;
-	i = 0;
+    /* Parcourir la liste jusqu'à l'index ou la fin */
+    while (temp && i < idx - 1)
+    {
+        temp = temp->next;
+        i++;
+    }
 
-	while (temp != NULL && i < idx)
-	{
-		temp = temp->next;
-		i++;
-	}
+    /* Si l'index est invalide ou si on atteint la fin */
+    if (!temp || (i < idx - 1))
+        return (NULL);
 
-	if (temp == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
+    /* Insérer à la fin si temp->next est NULL */
+    if (temp->next == NULL)
+        return (add_dnodeint_end(h, n));
 
-	new_node->next = temp->next;
-	new_node->prev = temp;
-	if (temp->next != NULL)
-	{
-		temp->next->prev = new_node;
-	}
+    /* Créer le nouveau nœud */
+    new_node = malloc(sizeof(dlistint_t));
+    if (!new_node)
+        return (NULL);
 
-	temp->next = new_node;
+    new_node->n = n;
 
-	return (new_node);
+    /* Ajuster les pointeurs pour insérer le nouveau nœud */
+    new_node->next = temp->next;
+    new_node->prev = temp;
+    if (temp->next)
+        temp->next->prev = new_node;
+    temp->next = new_node;
+
+    return (new_node);
 }
