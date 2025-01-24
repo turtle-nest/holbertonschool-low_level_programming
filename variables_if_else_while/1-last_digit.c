ef0d57e421d7ac879dafe1b98a21e926f7b30e10
turtle-nest <n78693107@gmail.com>
2025-01-24 16:20:22 +0100

    Fix incrementation

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,23 +11,23 @@
  */
 int main(void)
 {
-int n;
-int last_digit;
+	int n;
+	int last_digit;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-last_digit = n % 10;
+	last_digit = n % 10;
 
-printf("Last digit of %d is ", n);
+	printf("Last digit of %d is ", n);
 
-if (last_digit > 5)
-printf("%d and is greater than 5\n", last_digit);
-else if (last_digit == 0)
-printf("%d and is 0\n", last_digit);
-else
-printf("%d and is less than 6 and not 0\n", last_digit);
+	if (last_digit > 5)
+		printf("%d and is greater than 5\n", last_digit);
+	else if (last_digit == 0)
+		printf("%d and is 0\n", last_digit);
+	else
+		printf("%d and is less than 6 and not 0\n", last_digit);
 
-return (0);
+	return (0);
 }
 
