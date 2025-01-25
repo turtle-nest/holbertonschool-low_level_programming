e7cd837f7d2ddc781bc2e5365aee4182ded91499
turtle-nest <n78693107@gmail.com>
2025-01-25 12:46:45 +0100

    Arrangement of files

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -14,21 +14,22 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-	for (i = 1; i <= size; i++)
-	{
-	for (j = size - i; j > 0; j--)
-	{
-	_putchar(' ');
-	}
-	for (k = 0; k < i; k++)
-	{
-	_putchar('#');
-	}
-	_putchar('\n');
-	}
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size - i; j > 0; j--)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k < i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 	else
 	{
-	_putchar('\n');
+		_putchar('\n');
 	}
+	return;
 }
